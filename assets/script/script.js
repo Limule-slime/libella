029b6f040d1919d578ca2851c9e2c2789f6eaf48
s733v3 <steeve@awebi.com>
2025-04-28 09:27:47 +0300

    Animation + fonction

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,10 +8,47 @@ function navMobile() {
 
 }
 
+function stickyHeader() {
+
+    $(window).on('scroll', function(){
+
+        if ( $(window).scrollTop() > 125 ) {
+            $('body').addClass('sticky');
+        } else {
+            $('body').removeClass('sticky');
+        }
+
+    })
+}
+
+function scrollTop() {
+
+    $(window).on('scroll', function(){
+
+        if ( $(window).scrollTop() > 800 ) {
+            $('.scroll-top-top').addClass('show');
+        } else {
+            $('.scroll-top-top').removeClass('show');
+        }
+
+    })
+
+    $('.scroll-top-top').on('click', function(){
+        $('body, html').scrollTop(0);
+        return false;
+    })
+}
+
 jQuery(document).ready(function($) {
 
     // Open nav mobile
     navMobile();
+
+    // Sticky header
+    stickyHeader();
+
+    // Scroll to top
+    scrollTop();
 
     // Animoate on scroll
     AOS.init({
